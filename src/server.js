9edef72b22e7476c00fb8cd7b46607ac3600fbb4
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-07-29 14:27:53 +0100

    fix(GRAN-111): Remove local proxy setup

--- src/server.js
+++ src/server.js
@@ -6,11 +6,9 @@ import { requestLogger } from '#/plugins/request-logger.js'
 import { failAction } from '#/common/helpers/fail-action.js'
 import { pulse } from '#/plugins/pulse.js'
 import { requestTracing } from '#/plugins/request-tracing.js'
-import { setupProxy } from '#/common/helpers/proxy/setup-proxy.js'
 import { metrics } from '@defra/cdp-metrics'
 
 export async function createServer() {
-  setupProxy()
   const server = Hapi.server({
     host: config.get('host'),
     port: config.get('port'),
